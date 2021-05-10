3bde19d48275913e47f77deb2a3771954e2f21db
AndyMc3000 <andypatmcd@gmail.com>
2021-05-10 20:27:23 +0000

    Add template for player_homt.html to test connection to MongoDB

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import os
-from flask import Flask
+from flask import (
+    Flask, flash, render_template,
+    redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
 if os.path.exists("env.py"):
@@ -12,14 +14,16 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+mongo = PyMongo(app)
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+@app.route("/user")
+def user():
+    user = mongo.db.users.find()
+    return render_template("player_home.html", user=user)
 
 
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
             debug=True)
-    
