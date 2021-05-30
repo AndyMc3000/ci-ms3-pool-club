717922f6399e754a069dd022dc6b7e011174b7d5
AndyMc3000 <andypatmcd@gmail.com>
2021-05-30 12:21:43 +0000

    Corrected errors in script.js having ran code through JSHint validator

--- static/js/script.js
+++ static/js/script.js
@@ -10,26 +10,26 @@ $(document).ready(function() {
 Bootstrap validation
 */
 
-// Example starter JavaScript for disabling form submissions if there are invalid fields
+// JavaScript for disabling form submissions if there are invalid fields
 (function () {
-  'use strict'
+  'use strict';
 
-  // Fetch all the forms we want to apply custom Bootstrap validation styles to
-  var forms = document.querySelectorAll('.needs-validation')
+  // Fetch all forms to apply custom Bootstrap validation styles to
+  var forms = document.querySelectorAll('.needs-validation');
 
-  // Loop over them and prevent submission
+  // Loop over forms and prevent submission
   Array.prototype.slice.call(forms)
     .forEach(function (form) {
       form.addEventListener('submit', function (event) {
         if (!form.checkValidity()) {
-          event.preventDefault()
-          event.stopPropagation()
+          event.preventDefault();
+          event.stopPropagation();
         }
 
-        form.classList.add('was-validated')
-      }, false)
-    })
-})()
+        form.classList.add('was-validated');
+      }, false);
+    });
+})();
 
 /*
  * EmailJS code for sending a user query from the contact form on index.html by email
